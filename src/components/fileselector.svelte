19049a226d982271fd14e9302113ae8875d7646e
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-02 21:45:46 +0200

    Kinda basic gallery completed

--- src/components/fileselector.svelte
+++ src/components/fileselector.svelte
@@ -1,28 +1,36 @@
 <script>
-import { query_selector_all } from "svelte/internal";
 
 
-    let test=4
     let files=false
+    let gallery=[]
+
     
+
     $: {
         if(files) {
             files = [...files]
-            files.forEach(file => console.log(file.type))
             files = files.filter(file => file.type.startsWith("image/"))
+            updateGallery();
         }
     }
-    function onButtonDown() {
+
+    function updateGallery() {
+        gallery = gallery.concat(files);
         files=false;
     }
 
-    function getColCount() {
-        let width = screen.width
-        return "columns-" + Math.floor(width/200)
+    function removeFiles() {
+        gallery = [];
+        console.log(gallery.length)
     }
+
+    function addFiles() {
+        document.getElementById('folderselect').click();
+    }
+
 </script> 
 
-{#if !files}
+{#if gallery.length < 1}
 <div class="max-w-xl">
     <label
         class="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
@@ -41,15 +49,33 @@ import { query_selector_all } from "svelte/internal";
     </label>
     <br>    
 </div>
+
 {:else} 
+
 <div>
-    <h1> Selected Files </h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={onButtonDown}> Remove files </button>
-    <div class={getColCount()}>
-        {#each files as file}
-        <img src="{URL.createObjectURL(file)}" alt="" width="200" height="200"/>
+    <h1 class="text-center text-2xl font-bold"> Gallery </h1>
+    <div class="flex items-center justify-center p-3 gap-3">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={removeFiles}> Remove All Images </button>
+        <label> 
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={addFiles}> Add Images </button>
+        <input id="folderselect" type="file" name="file_upload" class="hidden" webkitdirectory directory bind:files accept="image/*">
+        </label>
+    </div>
+    <div class="flex flex-wrap justify-center gap-2">
+        {#each gallery as image}
+        <div class="border-2 rounded-md" id="imageDiv" style="width: {Math.floor(screen.width/5) - 5}px; height: {Math.floor(screen.width/5) - 5}px">
+            <img src="{URL.createObjectURL(image)}" alt=""/>
+        </div>
         {/each}
     </div>
 </div>
 {/if}
+
+<style> 
+img {
+    height: 100%;
+    width: 100%;
+    object-fit: contain;
+}
+</style>
 
