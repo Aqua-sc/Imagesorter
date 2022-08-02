90e1ffdd06cfa3cc6719b022839f3e0d55b1ce9a
Inti Danschutter <inti.adegem@gmail.com>
2022-08-02 22:15:05 +0200

    css change to dropzone

--- src/components/fileselector.svelte
+++ src/components/fileselector.svelte
@@ -33,19 +33,19 @@
 {#if gallery.length < 1}
 <div class="max-w-xl">
     <label
-        class="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+        class="flex justify-center w-full h-32 px-4 transition bg-zinc-800 border-2 border-white-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
         <span class="flex items-center space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-teal-600" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
             </svg>
-            <span class="font-medium text-gray-600">
+            <span class="font-medium text-green-300">
                 Drop your folder here or
-                <span class="text-blue-600 underline">browse</span>
+                <span class="text-blue-700 underline">browse</span>
             </span>
         </span>
-        <input type="file" name="file_upload" class="hidden" webkitdirectory directory bind:files accept="image/*">
+        <input type="file" name="file_upload" class="hidden" webkitdirectory multiple bind:files accept="image/*">
     </label>
     <br>    
 </div>
@@ -58,7 +58,7 @@
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={removeFiles}> Remove All Images </button>
         <label> 
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={addFiles}> Add Images </button>
-        <input id="folderselect" type="file" name="file_upload" class="hidden" webkitdirectory directory bind:files accept="image/*">
+        <input id="folderselect" type="file" name="file_upload" class="hidden" webkitdirectory multiple bind:files accept="image/*">
         </label>
     </div>
     <div class="flex flex-wrap justify-center gap-2">
