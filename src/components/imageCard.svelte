c68ba7f9d02ec4c159c975188573c885a480ccd2
Inti Danschutter <inti.adegem@gmail.com>
2022-08-05 13:58:44 +0200

    Seperate class for gallery and imagecard

---
+++ src/components/imageCard.svelte
@@ -0,0 +1,20 @@
+<script> 
+    export let file
+    
+</script>
+
+<div class="border-2 rounded-md p-3 border-green-400">
+    <div class="border-double border-4 border-black rounded-md" id="imageDiv" style="width: {Math.floor(screen.width/5) - 5}px; height: {Math.floor(screen.width/5) - 5}px">
+        <img src="{URL.createObjectURL(file)}" alt=""/>
+    </div>
+    <p class="text-center break-all" style="width: {Math.floor(screen.width/5) - 5}px;"> <strong>Name:</strong> {file.name} </p>
+</div>
+
+<style> 
+    img {
+        height: 100%;
+        width: 100%;
+        object-fit: contain;
+    }
+</style>
+
