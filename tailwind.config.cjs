a94a1c8c49c1545a5b939ed0f25bd43bc3f002ad
Inti Danschutter <inti.adegem@gmail.com>
2022-08-10 17:01:06 +0200

    Scrollbar view Hover -> display name

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,4 +2,24 @@ module.exports = {
   mode: 'jit',
   purge: [ './src/**/*.svelte' ],
 
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
+  
+  theme: {
+    extend: {
+
+      colors: {
+        'bubblegum': '#ff77e9'
+      },
+
+      width: {
+        'pptx-image': '11%'
+      }
+      }
+  }
+  
+
 }
+
+
