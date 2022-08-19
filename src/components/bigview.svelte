fd4b5fc2b61a6417955aa721109405c62ace7a34
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-19 22:01:35 +0200

    Weird resize fixed :D

--- src/components/bigview.svelte
+++ src/components/bigview.svelte
@@ -8,16 +8,16 @@
 
 </script>
 <div class="flex p-3 gap-4">
-    <div class="flex h-selected w-selected min-w-selected min-h-selected border-4 border-rose-500 rounded-sm place-items-center"> 
+    <div class="flex h-selected w-selected border-4 border-rose-500 rounded-sm place-items-center"> 
         {#if file}
-            <img class="object-contain w-full h-full" src="{URL.createObjectURL(file)}" alt=""/>
+            <img class="object-contain h-full w-full" src="{URL.createObjectURL(file)}" alt=""/>
         {:else}
             <p class="text-center align-middle text-2xl"> No image selected </p>
         {/if}
     </div>
     {#if file}
-    <div>
-        <p><b> Name: </b> {file.name} </p>
+    <div class="w-selected-text">
+        <p class="break-words text-ellipsis"><b> Name: </b> {file.name} </p>
         <p><b> Size: </b> {humanFileSize(file.size)} </p>
     </div>
     {/if}
