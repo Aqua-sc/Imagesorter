b812d597cba961b1c9cc150abbe30745d4623bff
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-20 18:17:48 +0200

    layou category button changed

--- src/components/bigview.svelte
+++ src/components/bigview.svelte
@@ -41,18 +41,18 @@
 			<p><b> Size: </b> {humanFileSize(file.size)}</p>
 			<p><b> Dimensions: </b> {height} x {width}</p>
 			<div class="flex w-full text-center">
-				<p><b> Category: </b></p>
+				<p class="py-2"><b> Category: </b></p>
 				<button
 					class="flex relative inline-flex items-center justify-center p-1 mb-2 mr-2 ml-1 w-full text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
 				>
 					<span
-						class="relative px-2.5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-zinc-600 rounded-md group-hover:bg-opacity-0"
-					>
+						class="relative px-2.5 py-2.5 w w-full transition-all ease-in duration-75 bg-white bg-zinc-600 rounded-md group-hover:bg-opacity-0">
 						Default
-                        <svg aria-hidden="true" focusable="false"data-prefix="fas" data-icon="caret-down" class="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" >
-                            <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
-                        </svg>
+                        
 					</span>
+					<svg aria-hidden="true" focusable="false"data-prefix="fas" data-icon="caret-down" class="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" >
+						<path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+					</svg>
 				</button>
 			</div>
 		</div>
