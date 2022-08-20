f6e3bd7cbed5efe26f9ed4b8191091475a0e6a8b
Aqua <Inti.Danschutter@UGent.be>
2022-08-20 15:56:38 +0200

    Dimensions added, attempt at category-button

--- src/components/gallery.svelte
+++ src/components/gallery.svelte
@@ -64,26 +64,31 @@
     <div class="flex absolute left-0 gap-2 overscroll-contain overflow-hidden h-gallery">
         <div class="flex flex-col gap-2 p-2 overscroll-contain overflow-y-auto min-w-pptx-image flex-shrink-0">
             {#each gallery as image}
-                <div class="{selected_img === image ? "shadow-lg shadow-cyan-500/50  border-sky-400": "border-white"} border-4  rounded-md"> 
-                    <ImageCard on:selected={newSelect} file={image}/>
+                <div> 
+                    <ImageCard on:selected={newSelect} file={image} selected={selected_img === image}/>
                 </div>
             {/each}
         </div>
         <div class="overflow-hidden">
             <Bigview file={selected_img}/>
-            <div class="absolute inset-x-pptx-image bottom-0 p-3">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={removeFiles}> Remove All Images </button>
+            <div class="absolute inset-x-pptx-image bottom-1 py-1 px-2">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded" on:click={removeFiles} title="Remove All Files"> 
+                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path fill="none" d="M0 0h24v24H0z"/><path d="M7 4V2h10v2h5v2h-2v15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6H2V4h5zM6 6v14h12V6H6zm3 3h2v8H9V9zm4 0h2v8h-2V9z" fill="white"/>
+                    </svg> 
+                </button>
                 <label> 
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={addFiles}> Add Images </button>
-                <input id="folderselect" type="file" name="file_upload" class="hidden" webkitdirectory multiple bind:files accept="image/*">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded" on:click={addFiles} title="Add folder"> 
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path fill="none" d="M0 0h24v24H0z"/><path d="M11 11V7h2v4h4v2h-4v4h-2v-4H7v-2h4zm1 11C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z" fill="rgba(255,255,255,1)"/>
+                        </svg> 
+                    </button>
+                    <input id="folderselect" type="file" name="file_upload" class="hidden" webkitdirectory multiple bind:files accept="image/*">
                 </label>
             </div>
         </div>
     </div>
-</div>
-  
-    
-
+</div>      
 {/if}
 
 
