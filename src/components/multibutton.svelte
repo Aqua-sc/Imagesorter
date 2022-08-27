105fbb7e84e229d8ae5a1b987d54963a46ae688c
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-27 15:03:48 +0200

    Category button (no functionality) added

---
+++ src/components/multibutton.svelte
@@ -0,0 +1,42 @@
+<script>
+
+    let options = [ 
+                    { name: "Default", color: "white"},
+                    { name: "OPTIE 1", color: "teal-300"}, 
+                    { name: "OPTIE 2", color: "red-500"}, 
+                    { name: "OPTIE 3", color: "green-500"},
+                    { name: "OPTIE 4", color: "fuchsia-400"}
+                ];
+                
+    let show = false
+    let selected = "Default"
+   
+</script>
+
+
+
+
+<div class="relative inline-block">
+    <button class="flex relative  items-center justify-center  pr-4 w-50 mr-2 ml-1  text-sm font-medium text-gray-900 rounded-sm group bg-gradient-to-br from-cyan-500 to-blue-500" on:click={() => show = !show}>
+        <span class="relative px-2.5 py-2.5 transition-all ease-in duration-75bg-zinc-600 rounded-md group-hover:bg-opacity-0 text-white">
+            {selected}    
+        </span>
+        <svg aria-hidden="true" focusable="false"data-prefix="fas" data-icon="caret-down" class="w-2 ml-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"  >
+            
+            <path fill="white" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+            
+        </svg>
+    </button>
+    
+    {#if show}
+        <div class="bg-sky-300 bg-opacity-10 border-1 border-emerald-600 rounded-lg place-items-center">
+            {#each options as option}
+            <button class="flex gap-1 hover:bg-black hover:bg-opacity-5" on:click={() => selected = option.name}>
+                <div class="w-5 h-5 bg-{option.color} border-2 border-black"></div>
+                <p> {option.name} </p>
+            </button>
+            {/each}
+        </div>
+    {/if}
+</div>
+    
