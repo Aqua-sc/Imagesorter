a03b807ee2c2871889bdb07018b93812a1cc20b8
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-30 11:03:36 +0200

    Export -> correct zip-file name

--- src/components/exportbutton.svelte
+++ src/components/exportbutton.svelte
@@ -12,9 +12,9 @@
     function download() {
         if (name) {
             let zip = new JSZip();
-            files.forEach(a => zip.file(`${name}/${a.categoryValue.name}/${a.file.name}`, a.file));
+            files.forEach(a => zip.file(`${a.categoryValue.name}/${a.file.name}`, a.file));
             zip.generateAsync({ type:  'blob'}).then(function (content) {
-                FileSaver.saveAs(content, 'download.zip');
+                FileSaver.saveAs(content, `${name}.zip`);
             });
         } else {
             alert("Folder name can't be empty")
@@ -26,6 +26,7 @@
 
 
 <div class="flex gap-1 mt-1 place-items-center">
+    <input class="p-1 border-blue-500 bg-blue-300 bg-opacity-95 border-2 rounded-md text-white font-bold placeholder-gray-200 text-base focus:outline-0 focus:ring-2" bind:value={name} placeholder="Folder name here"> 
     <label> 
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded" on:click={download} title="Download"> 
             <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -33,6 +34,6 @@
             </svg> 
         </button>    
     </label>
-    <input class="p-1 border-blue-500 bg-blue-300 bg-opacity-95 border-2 rounded-md text-white font-bold placeholder-gray-200 text-sm focus:outline-0 focus:ring-2" bind:value={name} placeholder="Folder name here"> 
+    
 </div>
     
