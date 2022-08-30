b7a68af52cfd68c443b95bb4d042d5642caee36e
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-08-30 11:06:06 +0200

    Category names changed xD

--- src/stores/categories.js
+++ src/stores/categories.js
@@ -4,10 +4,10 @@ import { writable } from 'svelte/store';
 function createCategories() {
     let arrayi = { 
         1 : { name: "Default", color: "white", id: 1},
-        2 : { name: "Drinken", color: "teal-300", id: 2}, 
+        2 : { name: "Drinks", color: "teal-300", id: 2}, 
         3 : { name: "Fruit", color: "red-500", id: 3}, 
-        4 : { name: "Bomen", color: "green-500", id: 4},
-        5 : { name: "Bloemen", color: "fuchsia-400", id: 5}
+        4 : { name: "Trees", color: "green-500", id: 4},
+        5 : { name: "Flowers", color: "fuchsia-400", id: 5}
     };
 
     let length = 5;
