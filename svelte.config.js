dbd27c37a85491dce662b4a30027d7960e2b52ec
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2022-09-01 12:42:28 +0200

    test pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+		
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        })
+    
+    }
 };
 
 export default config;
