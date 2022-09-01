bba344d69c4faf113921b86f3caf126bd6279f35
Inti Danschutter <inti.adegem@gmail.com>
2022-09-01 16:24:45 +0200

    github pages losing hope

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import adapter from "@sveltejs/adapter-static"; 
-// was "@sveltejs/adapter-auto"
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-
-/** @type {import(""@sveltejs/kit").Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-		
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs",
-			fallback: '200.html'
-        })
-    
-    }
+ // Consult https://github.com/sveltejs/svelte-preprocess
+ // for more information about preprocessors
+ preprocess: preprocess(),
+
+ kit: {
+ // hydrate the <div id="svelte"> element in src/app.html
+  adapter: adapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: null
+  })
+ }
 };
 
 export default config;
