9de6813e856900e1e38beff271001334c3d8245c
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2023-04-14 19:31:28 +0200

    base for realsies

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-
+ 
 const dev = process.argv.includes('dev');
-
+ 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      paths: {
-        base: dev ? '' : '/PictureSorter',
-      },
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/PictureSorter',
+    },
   }
 };
