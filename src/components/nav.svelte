8195dd3826ac996a02e6afd1a813294d64e2b706
Aqua-sc <inti.adegem@gmail.com>
2023-04-14 19:31:28 +0200

    base for realsies

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,5 +1,9 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <nav class="flex items-center justify-center w-full bg-zinc-800 h-nav">
-    <a class="mx-4 text-lg text-sky-400 font-bold flex" href="/">Home</a>
-    <a class="mx-4 text-lg text-sky-400 font-bold" href="/about">About</a>
-    <a class="mx-4 text-lg text-sky-400 font-bold" href="/instructions">Instructions</a>
+    <a class="mx-4 text-lg text-sky-400 font-bold flex" href="{base}/">Home</a>
+    <a class="mx-4 text-lg text-sky-400 font-bold" href="{base}/about">About</a>
+    <a class="mx-4 text-lg text-sky-400 font-bold" href="{base}/instructions">Instructions</a>
 </nav> 
