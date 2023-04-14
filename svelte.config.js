c21a1bf984224dcc8c3b4a77110fbb7386eadfeb
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2023-04-14 18:49:51 +0200

    added base but better

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 export default {
   kit: {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-	  base: "/PictureSorter/",
+      paths: {
+        base: dev ? '' : '/PictureSorter',
+      },
       pages: 'build',
       assets: 'build',
       fallback: null,
