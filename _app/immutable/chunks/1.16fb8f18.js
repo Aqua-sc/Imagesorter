77df9471feb22893f5b7bb00341a35146ba1c9dd
Aqua-sc <inti.adegem@gmail.com>
2023-04-14 19:54:56 +0200

    rebuild after changed home navigation

--- _app/immutable/chunks/1.16fb8f18.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.44d8caf3.js";export{t as component};
--- _app/immutable/chunks/paths.3b1030dd.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1tnhier)==null?void 0:s.base)??"/PictureSorter";var e;const a=((e=globalThis.__sveltekit_1tnhier)==null?void 0:e.assets)??t;export{a,t as b};
