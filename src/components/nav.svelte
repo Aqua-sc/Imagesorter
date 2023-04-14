fc7f8bb4bbf03341e2f628e07a993b37ad3be2d5
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2023-04-14 20:22:46 +0200

    Update nav.svelte

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="flex items-center justify-center w-full bg-zinc-800 h-nav">
-    <a class="mx-4 text-lg text-sky-400 font-bold flex" href="{base}">Home</a>
+    <a class="mx-4 text-lg text-sky-400 font-bold flex" href="{base}/">Home</a>
     <a class="mx-4 text-lg text-sky-400 font-bold" href="{base}/about">About</a>
     <a class="mx-4 text-lg text-sky-400 font-bold" href="{base}/instructions">Instructions</a>
 </nav> 
