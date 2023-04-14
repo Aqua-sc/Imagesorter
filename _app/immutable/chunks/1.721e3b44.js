4f9c852554f7f4c93cae895edc0e8465dff57e98
Aqua-sc <inti.adegem@gmail.com>
2023-04-14 20:29:44 +0200

    added trailingslash

--- _app/immutable/chunks/1.721e3b44.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c71e77ed.js";export{t as component};
---
+++ _app/immutable/chunks/1.8585870d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.c0083f96.js";export{t as component};
