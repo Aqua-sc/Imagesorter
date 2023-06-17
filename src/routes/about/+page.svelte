95c170b0154d253e6f0bb36536211f8051213c30
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2023-06-17 11:44:02 +0200

    simple about page created

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,14 @@
-<svelte:head>
-    <title>Svelte Kit Pokedex - About</title>
-</svelte:head> 
+<script>
+    import { base } from '$app/paths';
+</script>
 
-
-<h1> About </h1>
-<p>This is a really cool Svelte kit ....</p>
+<div class="text-gray-200 text-center pt-">
+    <br>
+    <h1 class="text-3xl pb-3"> <u>About this website</u> </h1>
+    <p>This website was made with the goal of easing the process of sorting a bunch of (e.g. holiday-)pictures</p>
+    <p>It is your device that processes the data, nothing is sent to any server!</p>
+    <p>Make sure to check the <a href="{base}/instructions"><u>instructions</u></a> page to get an idea of what is possible</p>
+    <br>
+    <p>Any bugs or suggestions can be communicated to me using <a href="https://github.com/Aqua-sc/PictureSorter/issues/new"><u>a Github issue</u></a></p>
+    
+</div>
